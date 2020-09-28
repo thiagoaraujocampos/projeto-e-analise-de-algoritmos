85d3b34a49cbac94ea5daee1190a713043e25daf
Thiago Araujo Campos <thiagoaraujocampos@outlook.com>
2020-09-28 00:21:19 +0000

    run.codes não aceita -> partimos para C

--- processo-seletivo.py
+++ processo-seletivo.py
@@ -1,7 +1,35 @@
 def soluciona(K, C, R):
-	R.sort(reverse=True)
+	mergeSort(R)
 	return R[K-1]
 
+def mergeSort(vet): 
+	if len(vet) > 1: 
+		i = j = k = 0
+		meio = len(vet)//2
+		Esq = vet[:meio]  
+		Dir = vet[meio:]
+		mergeSort(Esq) 
+		mergeSort(Dir)
+  
+		while i < len(Esq) and j < len(Dir): 
+			if Esq[i] > Dir[j]: 
+				vet[k] = Esq[i] 
+				i+= 1
+			else: 
+				vet[k] = Dir[j] 
+				j+= 1
+			k+= 1
+         
+		while i < len(Esq): 
+			vet[k] = Esq[i] 
+			i+= 1
+			k+= 1
+          
+		while j < len(Dir): 
+			vet[k] = Dir[j] 
+			j += 1
+			k += 1
+						
 if __name__ == '__main__':
 	N = int(input())
 	R = []
@@ -14,7 +42,7 @@ if __name__ == '__main__':
 
 		R.clear()
 		for j in linha:
-			R.append(float(j))
+			R.append(float(j))    
 
 		del R[0]
 		del R[0]
