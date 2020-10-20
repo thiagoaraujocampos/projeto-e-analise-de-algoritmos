47ce852ab12b42ffcf7b611edab9acdbd6b3964a
Thiago Araujo Campos <thiagoaraujocampos@outlook.com>
2020-10-20 16:52:23 -0300

    t03 - dijkstra based problem solved

--- Em trenós/em-trenos.cpp
+++ Em trenós/em-trenos.cpp
@@ -1,85 +1,123 @@
-// Thiago Araujo Campos - 769850
+#include <limits.h>
+#include <stdio.h> 
+#include <float.h>
 
-#include <iostream>
-using namespace std;
+/*
+9 12 4 2
+0 1 1.3 0 5 1.5 0 4 3.2 1 2 0.7 1 3 0.3 2 3 2.3 4 9 0.2 5 6 1.7 5 7 3.0 6 7 0.9 6 8 2.1 7 8 1.1
+8 7 7 5 5 8 2 3
+2
+8
+*/
 
-#define max 4294967295
+int minDistance(double dist[], int tam, bool sptSet[]) 
+{ 
+	double min = DBL_MAX, min_index; 
+	for (int v = 0; v < tam; v++) 
+		if (sptSet[v] == false && dist[v] <= min) 
+			min = dist[v], min_index = v; 
+	return min_index; 
+} 
 
-int shortestPath(double c[], int tam, bool f[]) {
-  int i, index; 
-  double min = max;
+void dijkstra(double graph[], int vent[], int N, int tam, int src) 
+{ 
+	double dist[tam];
+	bool sptSet[tam];
 
-  for (i=0; i<tam; i++) {
-    if (f[i] == false and c[i] <= min) {
-      min = c[i];
-      index = i;
-    }
-  }
+	double resultado[2];
 
-  return index;
-}
+	double copia[tam][tam];
 
-double dijkstra(double *grafo, int tam, int inicio) {
+	for (int i=0; i<tam; i++) {
+		for (int j=0; j<tam; j++) {
+			copia[i][j] = *(graph + i*tam + j);
+		}
+	}
 
-  /*printf("\n\n%d %d\n\n", tam, inicio);
-  for (int i=0; i<tam; i++) {
-    for (int j=0; j<tam; j++) {
-      printf("%.1lf ", *(grafo + i*tam + j));
-    }
-    printf("\n");
-  }*/
+	for (int i = 0; i < tam; i++) 
+		dist[i] = DBL_MAX, sptSet[i] = false;
 
-  int i;
-  double c[tam];
-  bool f[tam];
+	dist[src] = 0.0; 
 
-  for (i=0; i<tam; i++) {
-    c[i] = max;
-    f[i] = false;
-  }
+	for (int count = 0; count < tam - 1; count++) { 
+		int u = minDistance(dist, tam, sptSet); 
+ 
+		sptSet[u] = true; 
+ 
+		for (int v = 0; v < tam; v++) {
+			if (!sptSet[v] && copia[u][v] != 0.0 && dist[u] != DBL_MAX && dist[u] + copia[u][v] < dist[v]) {
+				dist[v] = dist[u] + copia[u][v]; 
+			}
+		}
+	} 
 
-  c[inicio-1] = 0;
+	resultado[0] = dist[0];
 
-  for (i=0; i<tam-1; i++) {
-    int atual = shortestPath(c, tam, f);
-    f[atual] = true;
+	for (int j=0; j<N*2; j=j+2) {
+		//printf("VENT? %.1lf\n", copia[vent[j]][vent[j+1]]);
+		if (copia[vent[j]][vent[j+1]] >= 1.0 || copia[vent[j]][vent[j+1]] <= 0.0) {
+			copia[vent[j]][vent[j+1]] = 1.0; 
+			copia[vent[j+1]][vent[j]] = 1.0;
+		}
+	}
 
-    for (int j=0; j<tam; j++) {
-      if (!f[atual] && *(grafo + atual*tam + j) != -1 && c[atual] != max && c[atual] + *(grafo + atual*tam + j) < c[j]) {
-        c[j] = c[atual] + *(grafo + atual*tam + j);
-      }
-    }
-  }
+	for (int i = 0; i < tam; i++) 
+		dist[i] = DBL_MAX, sptSet[i] = false; 
 
-  return c[0];
-}
+	dist[src] = 0.0;
 
-int main()
-{
+	for (int count = 0; count < tam - 1; count++) { 
+		int u = minDistance(dist, tam, sptSet); 
+ 
+		sptSet[u] = true; 
+ 
+		for (int v = 0; v < tam; v++) {
+			if (!sptSet[v] && copia[u][v] != 0.0 && dist[u] != DBL_MAX && dist[u] + copia[u][v] < dist[v]) {
+				dist[v] = dist[u] + copia[u][v]; 
+			}
+		}
+	} 
+
+	resultado[1] = dist[0];
+
+	if (resultado[1] >= (resultado[0] - 0.01)) {
+		printf("victory\n");
+	} else {
+		printf("defeat\n");
+	}
+ 
+} 
+
+int main() 
+{ 
 	int M, E, N, C, i, j, x, y, inicio;
   double p;
   
   scanf("%d %d %d %d", &M, &E, &N, &C);
 
-  double mapa[M][M];
+  double mapa[M+1][M+1];
+	int vent[N*2];
 
-  for (i=0; i<M; i++) {
-    for (j=0; j<M; j++) {
-      mapa[i][j] = -1;
+  for (i=0; i<=M; i++) {
+    for (j=0; j<=M; j++) {
+      mapa[i][j] = 0;
     }
   }
 
   for (i=0; i<E; i++) {
     scanf("%d %d %lf", &x, &y, &p);
-    printf("%d %d %.1lf ",x, y, p);
     mapa[x][y] = p;
     mapa[y][x] = p;
   }
 
-  scanf("%d", &inicio);
+	for (i=0; i<N*2; i++) {
+		scanf("%d", &vent[i]);
+	}
 
-  double fim = dijkstra(mapa[0], M, inicio);
-  printf("%.1lf", fim);
+	for (i=0; i<C; i++) {
+		scanf("%d", &inicio);	
+		dijkstra(&mapa[0][0], vent, N, M+1, inicio);
+	}
 
-	return 0;
-}
+	return 0; 
+} 
